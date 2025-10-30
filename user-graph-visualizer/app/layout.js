cb55f32bd197ca83368b4f19bc01c9568577b849
Aryan Jain <66124615+Aryan-jain07@users.noreply.github.com>
2025-10-30 16:49:28 +0530

    Update layout.js

--- user-graph-visualizer/app/layout.js
+++ user-graph-visualizer/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "geist/font";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -18,10 +18,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
+
