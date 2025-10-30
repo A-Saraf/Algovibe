0c2fac4ac595e7e069f7411c2724c00f8006c38d
A-Saraf <avyavroll19@gmail.com>
2025-10-30 15:20:03 +0530

    Algorithm

---
+++ Algorithm.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dfs(int node, vector<vector<int>> &adj, vector<bool> &visited) {
+    visited[node] = true;
+    for (int neighbor : adj[node]) {
+        if (!visited[neighbor]) {
+            dfs(neighbor, adj, visited);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n + 1);  // adjacency list
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<bool> visited(n + 1, false);
+    int components = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            components++;
+            dfs(i, adj, visited);
+        }
+    }
+
+    cout << components << endl;
+    return 0;
+}
